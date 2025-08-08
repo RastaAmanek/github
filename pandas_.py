2409389567d88e9198b7773f04ebab0ee38e53b2
RastaAmanek <naprzodwieszowa@gmail.com>
2025-08-08 18:31:07 +0200

    pandas part 1

--- pandas_.py
+++ pandas_.py
@@ -4,11 +4,22 @@ import numpy as np
 array_ = np.array([1, 2, 3, 4, 5])
 print(pd.Series([1, 2, 3, 4, 5]))
 
-my_dict = {"a": [12, 23, 34], "b": [45, 56, 67]}
-my_df = pd.DataFrame(my_dict, index=[1, 2, 3])
-print(my_df)
+# my_dict = {"a": [12, 23, 34], "b": [45, 56, 67]}
+# my_df = pd.DataFrame(my_dict, index=[1, 2, 3])
+# print(my_df)
+#
+# print(my_df.values)
 
-print(my_df.values)
+# df = pd.read_csv("cars.csv")
+# print(df)
 
-df = pd.read_csv("cars.csv")
-print(df)
+# array_2 = np.array(range(1, 100))
+# print(pd.Series(array_2, index=array_2))
+#
+# my_dict_2 = {"a": [12, 23, 34], "b": [45, 56, 67], "c": [78, 89, 91], "d": [4578, 25, 4]}
+# my_df_2 = pd.DataFrame(my_dict_2, index=[1, 2, 3])
+# print(my_df_2)
+#
+# df = pd.read_csv("cars.csv", usecols=["title", "price"])
+# print(df.head(7))
+
