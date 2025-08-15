26f02114c73e179b414f71e4bcede662ea570236
RastaAmanek <naprzodwieszowa@gmail.com>
2025-08-15 16:09:40 +0200

    pandas part 2

---
+++ zrodla_danych.py
@@ -0,0 +1,14 @@
+
+# with open("tekst_3.txt", "w") as file_obj:
+#     file_obj.writelines(["Ala Ty kurwo\n", "jestes jebana szmata\n"])
+
+with open("text.txt", "r") as file_obj:
+    content = file_obj.read()
+
+# with open("text_2.txt", "r") as file_obj:
+#     content_2 = file_obj.read()
+
+print(1)
+print(content)
+print(content_2)
+print(3)
